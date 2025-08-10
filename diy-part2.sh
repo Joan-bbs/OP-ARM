7c174c0fc930f842bc03ea674e630ab71f0853cc
Joan-bbs <liuxing0726@gmail.com>
2025-08-10 23:29:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -59,9 +59,9 @@ sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/foot
 #merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-filebrowser
 
 # mosdns
-rm -rf feeds/kenzo/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns.git feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/kenzo/luci-app-mosdns
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns.git feeds/luci/applications/luci-app-mosdns
 
 # 修改主题背景
 cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
